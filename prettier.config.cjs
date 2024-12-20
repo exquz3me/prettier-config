975defedd7ab326423fdedd0ecc04d9308340e52
exquz3me <sint8x6@gmail.com>
2024-12-20 21:36:18 +0100

    change to cjs

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/**
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  $schema: 'http://json.schemastore.org/prettierrc',
+  singleQuote: true,
+  bracketSameLine: true,
+  arrowParens: 'avoid',
+};
