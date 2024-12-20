28b42a8acdcd063e0126b3838b701d8c41a9e03b
exquz3me <sint8x6@gmail.com>
2024-12-20 23:05:26 +0100

    changed to v3 using cjs

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
-  $schema: "http://json.schemastore.org/prettierrc",
+  $schema: 'http://json.schemastore.org/prettierrc',
   singleQuote: true,
   bracketSameLine: true,
-  arrowParens: "avoid",
+  arrowParens: 'avoid',
   useTabs: false,
   editorconfig: true,
 };
 
 // eslint-disable-next-line import/no-default-export -- required for Prettier
-export default config;
+module.exports = config;
